2c92851b2d9529e2f425c1cbdf7f19d98f09df68
= <=>
2024-12-21 01:36:23 +0800

    make the entrypoint class based

--- cron/sync_xml.php
+++ cron/sync_xml.php
@@ -2,12 +2,57 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
 use App\Services\XmlProcessor;
 
-$directory =  __DIR__ . '/../data/xml_files'; // Path to the XML files directory
-$processedDirectory = __DIR__ . '/../data/xml_files_processed';
-$processor = new XmlProcessor($directory, $processedDirectory);
+class XmlFileHandler
+{
+    private $xmlDirectory;
+    private $processedDirectory;
+    private $processor;
 
-echo "<pre>";
-var_dump($processor->process());
+    public function __construct(string $xmlDirectory, string $processedDirectory)
+    {
+        $this->xmlDirectory = $xmlDirectory;
+        $this->processedDirectory = $processedDirectory;
+        $this->processor = new XmlProcessor($this->xmlDirectory, $this->processedDirectory);
+    }
+
+    public function validateDirectories(): bool
+    {
+        return is_dir($this->xmlDirectory) && is_dir($this->processedDirectory);
+    }
+
+    public function processFiles(): void
+    {
+        try {
+            $result = $this->processor->process();
+            $this->displayResult($result);
+        } catch (Exception $e) {
+            $this->handleError($e->getMessage());
+        }
+    }
+
+    private function displayResult($result): void
+    {
+        echo "<pre>" . htmlspecialchars(print_r($result, true)) . "</pre>";
+    }
+
+    private function handleError(string $message): void
+    {
+        echo "Error: " . $message . "\n";
+    }
+}
+
+// Initialize the handler with directories
+$xmlDirectory = __DIR__ . '/../data/xml_files';
+$processedDirectory = __DIR__ . '/../data/xml_files_processed';
+
+$fileHandler = new XmlFileHandler($xmlDirectory, $processedDirectory);
+
+// Validate directories and process files
+if (!$fileHandler->validateDirectories()) {
+    echo "Error: One or more directories do not exist.\n";
+    exit;
+}
+
+$fileHandler->processFiles();
